82bbf31075284e9d1cd2effe0a3ad71a3332e101
Lama <zerin108@gmail.com>
2016-04-03 15:09:13 +0600

    First commit

---
+++ application/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lama
+ * Date: 16.03.2016
+ * Time: 14:39
+ *
+ * file activate the application
+ */
+
+require_once 'core/model.php';
+require_once "core/view.php";
+require_once 'core/controller.php';
+require_once 'core/route.php';
+
+Route::start();
+
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lama
+ * Date: 16.03.2016
+ * Time: 11:10
+ */
+#ini configs
+#enable displaying errors in generated markup
+ini_set('display_errors', 1);
+require_once "application/bootstrap.php";
+
